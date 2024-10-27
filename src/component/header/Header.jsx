1da7ce19d9aef5d1c8387f5ebd3f421330535a62
rajibhossainimran <rajibhossainimran@gmail.com>
2024-10-27 22:07:48 +0600

    header section complete

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -39,8 +39,7 @@ const Header = () => {
     </ul>
   </div>
   <div className="navbar-end">
-  <label className="input input-bordered flex items-center gap-2 rounded-3xl">
-  
+  <label className="input input-bordered flex items-center gap-2 rounded-3xl w-1/2">
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
@@ -51,9 +50,9 @@ const Header = () => {
       d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
       clipRule="evenodd" />
   </svg>
-  <input type="text" className="grow =" placeholder="Search" />
+  <input type="text" className="grow w-1/3" placeholder="Search" />
 </label>
-  <div className="w-10 rounded-full">
+  <div className="w-10 rounded-full ms-3">
           <a href="#">
           <img className="rounded-full"
             alt="Tailwind CSS Navbar component"
