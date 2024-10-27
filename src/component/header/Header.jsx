3094a6de7b8a206fef1dfd1c6d165334d82ec327
rajibhossainimran <rajibhossainimran@gmail.com>
2024-10-27 17:51:11 +0600

    navber user logo added

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -22,28 +22,37 @@ const Header = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        <li className="text-base"><a>Home</a></li>
+        <li className="text-base"><a>Recipes</a></li>
+        <li className="text-base"><a>About</a></li>
+        <li className="text-base"><a>Search</a></li>
       </ul>
     </div>
-    <a className="text-2xl font-bold" href="#">Recipe Calories</a>
+    <a className="text-2xl font-bold whitespace-nowrap" href="#">Recipe Calories</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-      <li><a>Recipes</a></li>
-      <li><a>About</a></li>
-      <li><a>Search</a></li>
+      <li className="text-base"><a>Home</a></li>
+      <li className="text-base"><a>Recipes</a></li>
+      <li className="text-base"><a>About</a></li>
+      <li className="text-base"><a>Search</a></li>
     </ul>
   </div>
   <div className="navbar-end">
+  <label className="input input-bordered flex items-center gap-2 rounded-3xl">
+  
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      clipRule="evenodd" />
+  </svg>
+  <input type="text" className="grow =" placeholder="Search" />
+</label>
   <div className="w-10 rounded-full">
           <a href="#">
           <img className="rounded-full"
